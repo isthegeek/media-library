f9fb4567c2668e86aca03a19e66c1027cedbfb62
Rishi Raj <rishiraj824@gmail.com>
2016-06-24 15:36:47 +0530

    moved to user.js

--- js/media.user.js
+++ js/media.user.js
@@ -11,23 +11,7 @@
 
   Drupal.behaviors.MediaLibraryUser = {
     attach: function (context, settings) {
-      var $view = $('.view-content grid', context);
-      $view.once('media').prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
-
-      // Indicate that images are loading.
-      $view.append('<div class="ajax-progress ajax-progress-fullscreen">&nbsp;</div>');
-      $view.imagesLoaded(function () {
-        $view.masonry({
-          columnWidth: '.grid-sizer',
-          gutter: '.gutter-sizer',
-          itemSelector: '.grid-item',
-          percentPosition: true,
-          isFitWidth:true
-        });
-        // Add a class to reveal the loaded images, which avoids FOUC.
-        $('.grid-item').addClass('item-style');
-        $view.find('.ajax-progress').remove();
-      });
+      
     }
   };
 
